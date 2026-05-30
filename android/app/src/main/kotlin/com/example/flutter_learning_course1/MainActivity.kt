8e8ff8f4ea35cf3e4c14f9d896773194976b59f8
charann-p <cped0004@student.monash.edu>
2026-05-30 17:37:23 +1000

    First time creating a repository

---
+++ android/app/src/main/kotlin/com/example/flutter_learning_course1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_learning_course1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Home App'), centerTitle: true),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Account'),
+          ],
+          // onDestinationSelected: {
+          //   print(object)
+          // },
+          selectedIndex: 1,
+        ),
+        body: Center(),
+      ),
+    );
+  }
+}
